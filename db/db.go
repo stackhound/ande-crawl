8826634198b548240c4b4b97893ef94f6af7a383
Joel Gimenez <joelsinbarba@gmail.com>
2017-04-05 17:27:44 -0400

    making db communication  work

--- db/db.go
+++ db/db.go
@@ -1,7 +1,16 @@
 package db
 
 import (
+	//"encoding/json"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"log"
+	"os"
+)
+
+var (
+	mgoSession   *mgo.Session
+	databaseName = "stackhound"
 )
 
 // ConsumptionRecord represents a data structure for the JSON document to be stored.
@@ -9,16 +18,68 @@ type ConsumptionRecord struct {
 	NIS         string `json:"nis"`
 	Consumption int64  `json:"consumption"`
 	Amount      int64  `json:"amount"`
-	//CreatedAt   int64  `json:"created_at"` //
+	//CreatedAt   int64  `json:"created_at"`
 }
 
-// GetAvailableNIS returns an array of NIS records.
+type nis struct {
+	NIS string `json:"nis"`
+}
+
+type nises []nis //array of multiple nis
+
+// getSession defines cluster and starts the connection
+func getSession() *mgo.Session {
+	uri := "mongodb://joel:12345678@ds153400.mlab.com:53400/stackhound" //os.Getenv("MONGO_URL")
+	if uri == "" {
+		log.Println("No se pudo encontrar la url ")
+		os.Exit(1)
+	}
+
+	if mgoSession == nil {
+		var err error
+		mgoSession, err = mgo.Dial(uri)
+		if err != nil {
+			panic(err) // no, not really
+		}
+	}
+	return mgoSession.Clone()
+}
+
+// withCollection lets you pass collection name and query
+func withCollection(collection string, s func(*mgo.Collection) error) error {
+	session := getSession()
+	defer session.Close()
+	c := session.DB(databaseName).C(collection)
+	return s(c)
+}
+
+// GetAvailableNIS returns an array of NIS records from db
 func GetAvailableNIS() []string {
 	log.Println("Fetching NIS records.")
-
 	var records []string
+	var result nises
+	query := func(c *mgo.Collection) error {
+		err := c.Find(bson.M{}).All(&result)
+		if err != nil {
+			log.Fatal("Error al buscar el dato: ", err)
+			//return
+		}
+		return err
+	}
+	withCollection("nis", query)
+
+	for _, onenis := range result {
+
+		records = append(records, onenis.NIS) //add this nis to the array
+	}
+
+	/*str, _ := json.MarshalIndent(result, "", " ")
+	log.Printf("%s\n", str)*/
+
+	//log.Println("NIS : ", result.NIS)
+	//log.Println(availablenis)
 	// Sample values:
-	records = append(records, "1000", "1001")
+	records = append(records, "noesnis")
 
 	return records
 }
--- main.go
+++ main.go
@@ -3,27 +3,28 @@ package main
 import (
 	"log"
 
-	"github.com/stackhound/ande-crawl/crawl"
+	//"github.com/stackhound/ande-crawl/crawl"
 	"github.com/stackhound/ande-crawl/db"
 )
 
 func main() {
 	records := db.GetAvailableNIS()
+	log.Println(records)
+	/*
+		for _, nis := range records {
+			consumption, amount, err := crawl.FetchConsumption(nis)
 
-	for _, nis := range records {
-		consumption, amount, err := crawl.FetchConsumption(nis)
+			if err != nil {
+				log.Fatal("Couldn't fetch consumption!")
 
-		if err != nil {
-			log.Fatal("Couldn't fetch consumption!")
+				// Skip this:
+				continue
+			}
 
-			// Skip this:
-			continue
-		}
-
-		// No errors, store the record:
-		record := db.ConsumptionRecord{}
-		//record.Consumption = consumption
-		//record.Amount = amount
-		db.StoreConsumptionRecord(&record)
-	}
+			// No errors, store the record:
+			record := db.ConsumptionRecord{}
+			//record.Consumption = consumption
+			//record.Amount = amount
+			db.StoreConsumptionRecord(&record)
+		}*/
 }
